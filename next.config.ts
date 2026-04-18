29445a2e52d1e89baa9541e9a9f58d27db21562b
karanskhot <karanskhot@gmail.com>
2026-04-18 18:40:20 +0530

    package issues

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactCompiler: false,
-  async rewrites() {
-    return [
-      {
-        // Jab bhi frontend /api/v1/... call karega
-        source: '/api/v1/:path*',
-        // Toh Vercel usey chupke se Railway pe bhej dega
-        destination: 'https://ohhcode.up.railway.app*',
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
